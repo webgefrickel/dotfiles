be7f208e604d80ee2760fa2c94e464b7a3704bdd
Steffen Becker <webgefrickel@gmail.com>
2013-09-05 21:04:33 +0200

    brew default install software bash-script

---
+++ brew.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# NOTE install xcode first!
+
+# install brew packages
+brew install ack
+brew install composer
+brew install ctags
+brew install curl
+brew install encfs
+brew install fontforge
+brew install ghostscript
+brew install git
+brew install git-flow
+brew install jpegoptim
+brew install lynx
+brew install macvim
+brew install markdown
+brew install mcrypt
+brew install mercurial
+brew install mysql
+brew install node
+brew install optipng
+brew install phake
+brew install php55
+brew install php55-mcrypt
+brew install php55-xdebug
+brew install reattach-to-user-namespace
+brew install ssh-copy-id
+brew install subversion
+brew install the_silver_searcher
+brew install tmux
+brew install ttf2eot
+brew install unrar
+brew install vifm
+brew install wget
+brew install z
+brew install zsh
+
+# brew cask and install software
+
+brew cask install adium
+brew cask install alfred
+brew cask install amadeus-pro
+brew cask install carbon-copy-cloner
+brew cask install charles
+brew cask install doxie
+brew cask install dropbox
+brew cask install firefox
+brew cask install forklift
+brew cask install google-chrome
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install key-remap4-mac-book
+brew cask install libre-office
+brew cask install max
+brew cask install miro-video-converter
+brew cask install opera
+brew cask install pc-keyboardhack
+brew cask install sequel-pro
+brew cask install simple-comic
+brew cask install skype
+brew cask install slate
+brew cask install sourcetree
+brew cask install things
+brew cask install timings
+brew cask install transmission
+brew cask install true-crypt
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+
+# the rest -> appstore :-)
