5cf9fe2b929ea8cb23825ba1eead0fd3e93ea57e
Steffen Rademacker <kontakt@webgefrickel.de>
2015-06-18 12:04:12 +0200

    forklift plugin

--- zsh/forklift/forklift.plugin.zsh
+++ zsh/forklift/forklift.plugin.zsh
@@ -12,6 +12,13 @@
 # app window.
 #
 # https://gist.github.com/3313481
+#
+# Since Mavericks you have to allow assistive access in system preferences
+# -> privacy for System Events.app and ForkLift2.app, as well as osascript
+# using this command:
+#
+# sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','/usr/bin/osascript',1,1,1,NULL)"
+#
 function fl {
   if [ ! -z "$1" ]; then
     DIR=$1
@@ -24,46 +31,23 @@ function fl {
   fi
   osascript 2>&1 1>/dev/null <<END
 
-    try
-      tell application "Finder"
-        set appName to name of application file id "com.binarynights.ForkLift2"
-      end tell
-    on error err_msg number err_num
-      tell application "Finder"
-        set appName to name of application file id "com.binarynights.ForkLift"
-      end tell
-    end try
-
-    if application appName is running
-      tell application appName
-        activate
-      end tell
-    else
-      tell application appName
-        activate
-      end tell
-      repeat until application appName is running
-        delay 1
-      end repeat
-      tell application appName
-        activate
-      end tell
-    end if
-
-    tell application "System Events"
-      tell application process "ForkLift"
-        try
-          set topWindow to window 1
-        on error
-          keystroke "n" using command down
-          set topWindow to window 1
-        end try
-        keystroke "g" using {command down, shift down}
-        tell sheet 1 of topWindow
-          set value of text field 1 to "$PWD"
-          keystroke return
-        end tell
+  tell application "ForkLift"
+    activate
+  end tell
+  tell application "System Events"
+    tell application process "ForkLift"
+      try
+        set topWindow to window 1
+      on error
+        keystroke "n" using command down
+        set topWindow to window 1
+      end try
+      keystroke "g" using {command down, shift down}
+      tell sheet 1 of topWindow
+        set value of text field 1 to "$PWD"
+        keystroke return
       end tell
     end tell
+  end tell
 END
 }
