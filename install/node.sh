26f7642623818d3eaf8a87a410110e4488c4284b
Steffen Rademacker <kontakt@webgefrickel.de>
2015-07-29 19:58:16 +0200

    babel and testing-kram

--- install/node.sh
+++ install/node.sh
@@ -7,18 +7,23 @@ brew unlink node
 brew link node
 curl -L https://npmjs.org/install.sh | sh
 
+npm install -g babel
+npm install -g browser-run
+npm install -g browserify
 npm install -g caniuse-cmd
 npm install -g dploy
 npm install -g eslint
+npm install -g faucet
 npm install -g fkill-cli
 npm install -g frontend-md
 npm install -g grunt-cli
 npm install -g gulp
-npm install -g jsonlint
 npm install -g htmlhint
+npm install -g jsonlint
 npm install -g npm-check-updates
 npm install -g pure-prompt
 npm install -g recursive-blame
 npm install -g stylestats
+npm install -g tape
 npm install -g trash
 npm install -g vtop
