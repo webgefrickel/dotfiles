dedccaad0ed38cefa0b7f3aa613291c0278ade19
Steffen Rademacker <kontakt@webgefrickel.de>
2016-03-10 14:51:26 +0100

    update base install script, new cask-version etc.

--- install/_install.sh
+++ install/_install.sh
@@ -4,21 +4,20 @@
 # everything neccessary first - command line tools
 xcode-select --install
 
-# install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor
+read -p "Press any key to continue... " -n1 -s
 
-# tap the casks
-brew install caskroom/cask/brew-cask
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
 
 # brew paths
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 # install some neccessary brew packages
-brew install brew-cask
 brew install clipper
 brew install coreutils
+brew install fasd
 brew install git
 brew install tmux
 brew install zsh
@@ -31,18 +30,16 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 # this is the minimal software used
 brew cask install forklift
 brew cask install iterm2
+brew cask install owncloud
 
 # add a nice font
 brew cask install caskroom/fonts/font-hack
-
-# set zsh to the default
-sudo vim /etc/shells
-chsh -s /usr/local/bin/zsh
-/usr/bin/env zsh
-sudo mv /etc/zshenv /etc/zprofile
+brew linkapps
+brew tap homebrew/services
 
 # clipper
-ln -sfv /usr/local/opt/clipper/*.plist ~/Library/LaunchAgents
+mkdir -p ~/Library/LaunchAgents
+brew services start clipper
 
 # install all submodules
 git submodule init
@@ -52,7 +49,6 @@ git submodule update
 ln -s ~/dotfiles/agignore ~/.agignore
 ln -s ~/dotfiles/editorconfig ~/.editorconfig
 ln -s ~/dotfiles/eslintrc ~/.eslintrc
-ln -s ~/dotfiles/gemrc ~/.gemrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/slate.js ~/.slate.js
@@ -68,3 +64,9 @@ tic ~/dotfiles/terminfo/xterm-256color.terminfo
 
 # set iterm terminal to xterm-256color and enable italics!
 # now restart your terminal/iterm2 and hope for the best
+
+# set zsh to the default
+sudo vim /etc/shells
+chsh -s /usr/local/bin/zsh
+/usr/bin/env zsh
+sudo mv /etc/zshenv /etc/zprofile
