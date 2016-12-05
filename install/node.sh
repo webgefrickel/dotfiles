c4f187ee9206c598f4c4129a938a4cc8a4dbea7d
Steffen Rademacker <kontakt@webgefrickel.de>
2016-12-05 10:35:57 +0100

    update to node v6 + ditch unused npm modules, add caskup

--- install/node.sh
+++ install/node.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
 # use nvm instead of brew node or anything else
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-nvm install 4.6.0
-nvm use 4.6.0
-nvm alias default 4.6.0
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+nvm install 6.9.1
+nvm use 6.9.1
+nvm alias default 6.9.1
 curl -L https://www.npmjs.com/install.sh | sh
 
 npm update -g
@@ -13,12 +13,11 @@ npm install -g caniuse-cmd
 npm install -g diff-so-fancy
 npm install -g eslint
 npm install -g fkill-cli
-npm install -g grunt-cli
 npm install -g gulp
 npm install -g jsonlint
+npm install -g np
 npm install -g npm-check-updates
 npm install -g pure-prompt
-npm install -g recursive-blame
 npm install -g sass-lint
 npm install -g trash-cli
 npm install -g vtop
