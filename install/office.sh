0dd2c3ce3f3caac7f1245368af79432e7309a715
Steffen Rademacker <kontakt@webgefrickel.de>
2017-03-19 14:10:25 +0100

    Add install script for office/mutt/khal/khard etc.

---
+++ install/office.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# everything needed for mutt
+brew install isync
+brew install khal
+brew install khard
+brew install msmtp
+brew install mu
+brew install neomutt/homebrew-neomutt/neomutt --with-sidebar-patch
+brew install ripmime
+brew install w3m
+read -p "Press any key to continue... " -n1 -s
+
+# python dependencies (for mutt)
+sudo pip3 install -I urlscan
+read -p "Press any key to continue... " -n1 -s
+
+mkdir ~/Calendars
+mkdir ~/Contacts
+mkdir ~/Mail
+mkdir ~/Mail/mailbox
+mkdir ~/Mail/viu
+mkdir ~/Mail/mf
+mkdir ~/Mail/kfi
+mkdir ~/Mail/hsv
+read -p "Press any key to continue... " -n1 -s
+
+ln -s ~/dotfiles/office/mutt ~/.mutt
+ln -s ~/dotfiles/office/mbsyncrc ~/.mbsyncrc
+ln -s ~/dotfiles/office/msmtprc ~/.msmtprc
+ln -s ~/dotfiles/office/khal ~/.config/khal
+ln -s ~/dotfiles/office/khard ~/.config/khard
+ln -s ~/dotfiles/office/vdirsyncer ~/.vdirsyncer
+read -p "Press any key to continue... " -n1 -s
