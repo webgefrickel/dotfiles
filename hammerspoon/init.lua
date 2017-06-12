1118f71d782f4a3a31fa6053122aa75f419227c9
Steffen Rademacker <kontakt@webgefrickel.de>
2017-06-12 20:39:54 +0200

    Hammerspoon final config (for now)

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
 local tiling = require 'hs.tiling'
+local appliaction = require 'hs.application'
 local hyper = { 'cmd', 'alt', 'shift', 'ctrl' }
 local meta = { 'ctrl', 'cmd' }
 local keys = { 
@@ -93,6 +94,27 @@ end):start()
 -- Window management
 --------------------
 
+function isInScreen(screen, win)
+  return win:screen() == screen
+end
+
+function moveMouse()
+  local pt = hs.geometry.rectMidPoint(hs.window.focusedWindow():frame())
+  hs.mouse.setAbsolutePosition(pt)
+end
+
+function focusScreen(screen)
+  -- Get windows within screen, ordered from front to back.
+  -- If no windows exist, bring focus to desktop. Otherwise, set focus on
+  -- front-most application window.
+  local windows = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+  windowToFocus:focus()
+  moveMouse()
+end
+
 -- Push the window into the center of the screen
 local function center(window)
   frame = window:screen():frame()
@@ -103,18 +125,20 @@ local function center(window)
   window:setFrame(frame)
 end
 
-hs.hotkey.bind(hyper, '1', function() tiling.goToLayout('gp-vertical') end)
-hs.hotkey.bind(hyper, '2', function() tiling.goToLayout('fullscreen') end)
-hs.hotkey.bind(hyper, 'f', function() tiling.toggleFloat(center) end)
-hs.hotkey.bind(hyper, 'r', function() tiling.retile() end)
-hs.hotkey.bind(hyper, 'a', function() tiling.cycle(1) end)
-hs.hotkey.bind(hyper, 'w', function() tiling.promote() end)
-hs.hotkey.bind(hyper, 'c', function() tiling.cycleLayout() end)
-hs.hotkey.bind(hyper, '[', function() hs.window.focusedWindow():moveOneScreenNorth() end)
-hs.hotkey.bind(hyper, ']', function() hs.window.focusedWindow():moveOneScreenSouth() end)
-hs.hotkey.bind(hyper, 'h', function() hs.window.focusedWindow():focusWindowWest() end)
-hs.hotkey.bind(hyper, 'j', function() hs.window.focusedWindow():focusWindowSouth() end)
-hs.hotkey.bind(hyper, 'k', function() hs.window.focusedWindow():focusWindowNorth() end)
-hs.hotkey.bind(hyper, 'l', function() hs.window.focusedWindow():focusWindowEast() end)
+hs.hotkey.bind(hyper, '1', function() tiling.goToLayout('gp-vertical'); moveMouse() end)
+hs.hotkey.bind(hyper, '2', function() tiling.goToLayout('fullscreen'); moveMouse() end)
+hs.hotkey.bind(hyper, 'f', function() tiling.toggleFloat(center); moveMouse() end)
+hs.hotkey.bind(hyper, 'r', function() tiling.retile(); moveMouse() end)
+hs.hotkey.bind(hyper, 'a', function() tiling.cycle(1); moveMouse() end)
+hs.hotkey.bind(hyper, 'w', function() tiling.promote(); moveMouse() end)
+hs.hotkey.bind(hyper, 'c', function() tiling.cycleLayout(); moveMouse() end)
+hs.hotkey.bind(hyper, '[', function() hs.window.focusedWindow():moveOneScreenNorth(); moveMouse() end)
+hs.hotkey.bind(hyper, ']', function() hs.window.focusedWindow():moveOneScreenSouth(); moveMouse() end)
+hs.hotkey.bind(hyper, 'h', function() hs.window.focusedWindow():focusWindowWest(); moveMouse() end)
+hs.hotkey.bind(hyper, 'j', function() hs.window.focusedWindow():focusWindowSouth(); moveMouse() end)
+hs.hotkey.bind(hyper, 'k', function() hs.window.focusedWindow():focusWindowNorth(); moveMouse() end)
+hs.hotkey.bind(hyper, 'l', function() hs.window.focusedWindow():focusWindowEast(); moveMouse() end)
+hs.hotkey.bind(hyper, 'n', function () focusScreen(hs.window.focusedWindow():screen():next()) end)
+
 
 tiling.set('layouts', { 'fullscreen', 'gp-vertical', 'gp-horizontal', 'columns' })
