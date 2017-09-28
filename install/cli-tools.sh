2c0ed2dc20055baa9084725380ae7b26d06bc089
Steffen Rademacker <kontakt@webgefrickel.de>
2017-09-28 15:42:04 +0200

    PHP 7.1

--- install/cli-tools.sh
+++ install/cli-tools.sh
@@ -3,7 +3,7 @@ brew tap homebrew/php
 
 # install additional brew packages -- see _install.sh too
 brew install composer
-brew install curl --with-libssh2
+brew install curl --with-libssh2 --with-openssl
 brew install ffmpeg --with-libvpx --with-libvorbis --with-fdk-aac
 brew install git-flow
 brew install git-ftp
@@ -12,14 +12,17 @@ brew install mariadb
 brew install mozjpeg
 brew install nginx
 brew install openssl
-brew install php70 --with-homebrew-curl
-brew install php70-intl
-brew install php70-mcrypt
+brew install php71 --with-homebrew-curl 
+brew install php71-intl
+brew install php71-mcrypt
 brew install rbenv
 brew install rsync
 brew install shpotify
 brew install the_silver_searcher
 brew install youtube-dl
+
+# link keg-only / pre-installed duplicates
+brew link --force curl
 
 # install a current ruby version
 rbenv install 2.4.1
