8ed8b707760788d4cdfa2e22f169229f8e0ff055
Steffen Rademacker <kontakt@webgefrickel.de>
2018-11-10 15:21:51 +0100

    Change computer install name

--- install/osx.sh
+++ install/osx.sh
@@ -10,10 +10,10 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "mausohr"
-sudo scutil --set HostName "mausohr"
-sudo scutil --set LocalHostName "mausohr"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "mausohr"
+sudo scutil --set ComputerName "langohr"
+sudo scutil --set HostName "langohr"
+sudo scutil --set LocalHostName "langohr"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "langohr"
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
