8943308b6026c2343efd0a16be5d814bab3bc952
Steffen Rademacker <kontakt@webgefrickel.de>
2020-04-22 14:07:17 +0200

    npm-check + yarn start

--- install/node.sh
+++ install/node.sh
@@ -8,7 +8,7 @@ npm install -g diff-so-fancy
 npm install -g fkill-cli
 npm install -g localtunnel
 npm install -g neovim
-npm install -g npm-check-updates
+npm install -g npm-check
 npm install -g pure-prompt
 npm install -g trash-cli
 npm install -g yarn
