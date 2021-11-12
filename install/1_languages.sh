1a52da268247ef2e4e0ff78ced39a6802fee26cd
Steffen Rademacker <kontakt@webgefrickel.de>
2021-11-12 11:42:52 +0100

    Update python version

--- install/1_languages.sh
+++ install/1_languages.sh
@@ -20,7 +20,7 @@ npm update -g
 # first: find out the current python3 version in homebrew
 # then install this version with pyenv and link it
 brew install pyenv # restart terminal, then:
-pyenv install 3.9.7
-pyenv global 3.9.7
+pyenv install 3.9.8
+pyenv global 3.9.8
 pip install --upgrade pip
 pip install neovim
