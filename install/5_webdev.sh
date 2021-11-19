b4ef0994ed23ba450bc501b98ccdbc6f997724f6
Steffen Rademacker <kontakt@webgefrickel.de>
2021-11-19 20:16:36 +0100

    Update webdev software and etc-config (php 8, etc.)

--- install/5_webdev.sh
+++ install/5_webdev.sh
@@ -1,42 +1,32 @@
 #!/usr/bin/env zsh
 
-brew reinstall dnsmasq
-brew reinstall httpd
-brew reinstall imagemagick
-brew reinstall mariadb
-brew reinstall mkcert
-brew reinstall nss
-brew reinstall php@7.4
+brew install dnsmasq
+brew install httpd
+brew install imagemagick
+brew install mariadb
+brew install mkcert
+brew install nss
+brew install php
 
 # link etc-configs
-sudo rm -rf /usr/local/etc/php
-sudo rm -rf /usr/local/etc/httpd
-sudo rm /usr/local/etc/my.cnf
-sudo rm /usr/local/etc/dnsmasq.conf
-ln -s ~/dotfiles/etc/httpd /usr/local/etc/httpd
-ln -s ~/dotfiles/etc/php /usr/local/etc/php
-ln -s ~/dotfiles/etc/my.cnf /usr/local/etc/my.cnf
-ln -s ~/dotfiles/etc/dnsmasq.conf /usr/local/etc/dnsmasq.conf
+sudo rm -rf /opt/homebrew/etc/php
+sudo rm -rf /opt/homebrew/etc/httpd
+sudo rm /opt/homebrew/etc/my.cnf
+sudo rm /opt/homebrew/etc/dnsmasq.conf
+ln -s ~/dotfiles/etc/httpd /opt/homebrew/etc/httpd
+ln -s ~/dotfiles/etc/php /opt/homebrew/etc/php
+ln -s ~/dotfiles/etc/my.cnf /opt/homebrew/etc/my.cnf
+ln -s ~/dotfiles/etc/dnsmasq.conf /opt/homebrew/etc/dnsmasq.conf
 
 sudo rm /etc/hosts
 sudo ln -s ~/dotfiles/etc/resolver /etc/resolver
 sudo ln -f ~/dotfiles/etc/hosts /etc/hosts
 
-# php and ssl
-brew link php@7.4
-pecl install imagick
-
 # additional webdev cli tools
 npm install -g depcheck
-npm install -g glyphhanger
 npm install -g npm-check
 npm install -g npm-check-updates
 npm install -g yarn
-
-# additional stuff for glyphhanger
-pip install fonttools
-pip install brotli
-pip install zopfli
 
 # certificates for local ssl
 mkcert -install
@@ -47,7 +37,7 @@ mkcert localhost "dev.localhost" "*.dev.localhost"
 sudo brew services start dnsmasq
 brew services start httpd
 brew services start mariadb
-brew services start php@7.4
+brew services start php
 
 # mariadb
 sudo mysql -uroot
