e9eca451a0f3b6867db53f3629b42d2365f5696f
Steffen Rademacker <kontakt@webgefrickel.de>
2021-12-14 19:59:34 +0100

    Treesitter config

--- install/3_vimux.sh
+++ install/3_vimux.sh
@@ -11,7 +11,6 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # vim and tmux plugin managers
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # additional language-servers for nvim-lspconfig
 npm i -g neovim
@@ -21,7 +20,6 @@ npm i -g typescript-language-server
 npm i -g vscode-langservers-extracted
 npm i -g yaml-language-server
 
-vim +PlugInstall
-vim +TSInstall\ bash\ comment\ css\ html\ javascript\ jsdoc\ json\ json5\ lua\ php\ python\ regex\ ruby\ scss\ toml\ tsx\ typescript\ vue\ yaml
-vim +PlugUpgrade +PlugUpdate +TSUpdate
-vim +checkhealth
+# packer + treesitter
+nvim +PackerSync +TSUpdate
+nvim +checkhealth
