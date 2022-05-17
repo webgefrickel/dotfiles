47ccd30a9ecfbbe4c93393400bc74d0d57b44ace
Steffen Rademacker <kontakt@webgefrickel.de>
2022-05-17 11:30:21 +0200

    Fix git-seach conflict markers

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -93,8 +93,7 @@ map('n', '<leader>l', '<cmd>FloatermNew lazygit<cr>')
 map('n', '<leader>gd', '<cmd>DiffviewOpen<cr>')
 map('n', '<leader>gc', '<cmd>DiffviewClose<cr>')
 map('n', '<leader>gh', '<cmd>DiffviewFileHistory<cr>')
--- TODO rework this?!
-map('n', '<leader>gf', '/\\v^[<\\|=>]{7}( .*\\|$)<cr>')
+map('n', '<leader>gf', "<cmd>execute '/\\v^[<\\|=>]{7}/'<cr>")
 
 -- floaterm
 map('n', '-', '<cmd>FloatermNew nnn<cr>')
