213cf08fc8c758e003eba2e355c0c49c36821f3d
Steffen Rademacker <kontakt@webgefrickel.de>
2022-08-23 12:08:53 +0200

    Simpler neorg config

--- nvim/lua/config/neorg.lua
+++ nvim/lua/config/neorg.lua
@@ -1,13 +1,18 @@
 require('neorg').setup({
   load = {
     ["core.defaults"] = {},
+    ["core.norg.concealer"] = {},
+    ["core.norg.completion"] = {
+      config = {
+        engine = "nvim-cmp",
+      },
+    },
     ["core.norg.dirman"] = {
       config = {
         workspaces = {
-          work = "~/Documents/notes/work",
-          home = "~/Documents/notes/home",
-        }
-      }
-    }
-  }
+          default = "~/Notes",
+        },
+      },
+    },
+  },
 })
