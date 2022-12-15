b559370e2d3f6b335521a4ed30be7f6259281a36
Steffen Rademacker <kontakt@webgefrickel.de>
2022-12-15 15:16:14 +0100

    Upall and updates to lnags

--- install/1_languages.sh
+++ install/1_languages.sh
@@ -2,8 +2,8 @@
 
 # ruby
 brew install rbenv # restart terminal, then:
-rbenv install 3.1.2
-rbenv global 3.1.2
+rbenv install 3.1.3
+rbenv global 3.1.3
 
 # node
 curl -L https://git.io/n-install | bash
@@ -17,6 +17,6 @@ npm update -g
 # first: find out the current python3 version in homebrew
 # then install this version with pyenv and link it
 brew install pyenv # restart terminal, then:
-pyenv install 3.11.0
-pyenv global 3.11.0
+pyenv install 3.11.1
+pyenv global 3.11.1
 pip install --upgrade pip
