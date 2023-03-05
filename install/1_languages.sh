0b6f1081fc3b78b577b9d79494b3367398ec6ad0
Steffen Rademacker <kontakt@webgefrickel.de>
2023-03-05 12:02:47 +0100

    Update php, ruby, python

--- install/1_languages.sh
+++ install/1_languages.sh
@@ -2,8 +2,8 @@
 
 # ruby
 brew install rbenv # restart terminal, then:
-rbenv install 3.2.0
-rbenv global 3.2.0
+rbenv install 3.2.1
+rbenv global 3.2.1
 
 # node
 curl -L https://git.io/n-install | bash
@@ -19,6 +19,6 @@ npm update -g
 # first: find out the current python3 version in homebrew
 # then install this version with pyenv and link it
 brew install pyenv # restart terminal, then:
-pyenv install 3.11.1
-pyenv global 3.11.1
+pyenv install 3.11.2
+pyenv global 3.11.2
 pip install --upgrade pip
