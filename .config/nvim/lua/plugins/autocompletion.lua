451f0fbe8f15710c18d0456131dea48a3ba67635
Steffen Rademacker <kontakt@webgefrickel.de>
2025-02-28 11:25:19 +0100

    .

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -1,6 +1,7 @@
 return {
   { 'saghen/blink.cmp',
     dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
     opts = {
       keymap = { preset = 'super-tab' },
       sources = {
--- scripts/install.sh
+++ scripts/install.sh
@@ -25,8 +25,8 @@ sudo scutil --set LocalHostName "mausohr"
 sudo scutil --set HostName "mausohr"
 
 # some sane macos-default settings (minimum):
-defaults write -g InitialKeyRepeat -int 10
-defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 12
+defaults write -g KeyRepeat -int 2
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.Finder AppleShowAllFiles -bool true
 
@@ -57,8 +57,10 @@ npm install -g fkill-cli neovim npm npm-check trash-cli yarn
 cd ~/Dotfiles/scripts/out && npm i -g
 cd ~/Dotfiles/scripts/gallery && npm i -g
 
-# update bat to use local gruvbox-material-theme
+# some plugins and additional things to do 
+# (launch vim, install everything with lazy/mason...)
 bat cache --build
+ya pack -a yazi-rs/plugins:full-border
 
 # Neomutt and friends:create system-keychain-entries for the 
 # mailboxes (compare msmtp/mbsync-config, remember to prepend 
